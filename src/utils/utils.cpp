4b35b0f7bf9c1e60c46ced56496f629c1c933eea
SomePineaple <njweb@protonmail.com>
2022-08-07 13:20:42 -0400

    Obj Loader

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,7 +1,8 @@
 #include "utils.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
 
 char * Utils::readFileToString(const char * filename) {
   char * buffer = 0;
@@ -20,4 +21,82 @@ char * Utils::readFileToString(const char * filename) {
   }
 
   return buffer;
+}
+
+std::vector<Utils::Vertex> Utils::loadOBJ( std::istream& in ) {
+  std::vector<Vertex> verts;
+
+  std::vector< glm::vec4 > positions( 1, glm::vec4( 0, 0, 0, 0 ) );
+  std::vector< glm::vec3 > texcoords( 1, glm::vec3( 0, 0, 0 ) );
+  std::vector< glm::vec3 > normals( 1, glm::vec3( 0, 0, 0 ) );
+  std::string lineStr;
+  while( std::getline( in, lineStr ) ) {
+    std::istringstream lineSS( lineStr );
+    std::string lineType;
+    lineSS >> lineType;
+
+    // vertex
+    if( lineType == "v" )
+    {
+        float x = 0, y = 0, z = 0, w = 1;
+        lineSS >> x >> y >> z >> w;
+        positions.push_back( glm::vec4( x, y, z, w ) );
+    }
+
+    // texture
+    if( lineType == "vt" )
+    {
+        float u = 0, v = 0, w = 0;
+        lineSS >> u >> v >> w;
+        texcoords.push_back( glm::vec3( u, v, w ) );
+    }
+
+    // normal
+    if( lineType == "vn" )
+    {
+        float i = 0, j = 0, k = 0;
+        lineSS >> i >> j >> k;
+        normals.push_back( glm::normalize( glm::vec3( i, j, k ) ) );
+    }
+
+    // polygon
+    if( lineType == "f" ) {
+      std::vector< VertRef > refs;
+      std::string refStr;
+      while( lineSS >> refStr ) {
+        std::istringstream ref( refStr );
+        std::string vStr, vtStr, vnStr;
+        std::getline( ref, vStr, '/' );
+        std::getline( ref, vtStr, '/' );
+        std::getline( ref, vnStr, '/' );
+        int v = atoi( vStr.c_str() );
+        int vt = atoi( vtStr.c_str() );
+        int vn = atoi( vnStr.c_str() );
+        v  = (  v >= 0 ?  v : positions.size() +  v );
+        vt = ( vt >= 0 ? vt : texcoords.size() + vt );
+        vn = ( vn >= 0 ? vn : normals.size()   + vn );
+        refs.push_back( VertRef( v, vt, vn ) );
+      }
+
+      // triangulate, assuming n>3-gons are convex and coplanar
+      for( size_t i = 1; i+1 < refs.size(); ++i ) {
+        const VertRef* p[3] = { &refs[0], &refs[i], &refs[i+1] };
+
+        // http://www.opengl.org/wiki/Calculating_a_Surface_Normal
+        glm::vec3 U( positions[ p[1]->v ] - positions[ p[0]->v ] );
+        glm::vec3 V( positions[ p[2]->v ] - positions[ p[0]->v ] );
+        glm::vec3 faceNormal = glm::normalize( glm::cross( U, V ) );
+
+        for( size_t j = 0; j < 3; ++j ) {
+          Vertex vert;
+          vert.position = glm::vec3( positions[ p[j]->v ] );
+          vert.texcoord = glm::vec2( texcoords[ p[j]->vt ] );
+          vert.normal = ( p[j]->vn != 0 ? normals[ p[j]->vn ] : faceNormal );
+          verts.push_back( vert );
+        }
+      }
+    }
+  }
+
+  return verts;
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,8 +1,25 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <glm/glm.hpp>
+#include <vector>
+#include <iostream>
+
 namespace Utils {
   char * readFileToString(const char *);
+
+  struct Vertex {
+    glm::vec3 position;
+    glm::vec2 texcoord;
+    glm::vec3 normal;
+  };
+
+  struct VertRef {
+    VertRef(int v, int vt, int vn) : v(v), vt(vt), vn(vn) { }
+    int v, vt, vn;
+  };
+
+  std::vector<Vertex> loadOBJ(std::istream& in);
 }
 
 #endif // UTILS_h
