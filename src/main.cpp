31597bae440bbb4614263d9bb7fc4ada3c9abee9
SomePineaple <njweb@protonmail.com>
2022-08-07 12:12:24 -0400

    gl3w

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
 
 int main() {
@@ -11,8 +12,18 @@ int main() {
   GLFWwindow * window = glfwCreateWindow(800, 800, "GLGrass", nullptr, nullptr);
   glfwMakeContextCurrent(window);
 
+  if (gl3wInit()) {
+    std::cerr << "Failed to init gl3w" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  glClearColor(0.3, 0.5, 0.8, 1.0);
+
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
   }
   return EXIT_SUCCESS;
 }
